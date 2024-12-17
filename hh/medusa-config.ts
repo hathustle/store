b436e9980ebb6b8518364f5148a82e1fce3a6052
Kyle Rable <Krable55@gmail.com>
2024-12-16 21:11:11 -0800

    hardcode

--- hh/medusa-config.ts
+++ hh/medusa-config.ts
@@ -7,29 +7,30 @@ const DB_HOST = process.env.DB_HOST
 const DB_PORT = process.env.DB_PORT
 const DB_DATABASE = process.env.DB_DATABASE
 
-const DATABASE_URL =
-  `postgres://${DB_USERNAME}:${DB_PASSWORD}` +
-  `@${DB_HOST}:${DB_PORT}/${DB_DATABASE}`
+const REDIS_URL = "redis://default:ZhD9K3uw@hathustle_medusa-redis:6379"
+const DATABASE_URL = "postgres://postgres:FcDBTxd2@hathustle_db:5432/postgres"
+  // `postgres://${DB_USERNAME}:${DB_PASSWORD}` +
+  // `@${DB_HOST}:${DB_PORT}/${DB_DATABASE}`
 
 module.exports = defineConfig({
   modules: [
     {
       resolve: "@medusajs/medusa/cache-redis",
       options: {
-        redisUrl: process.env.REDIS_URL,
+        redisUrl: REDIS_URL, //process.env.REDIS_URL,
       },
     },
     {
       resolve: "@medusajs/medusa/event-bus-redis",
       options: {
-        redisUrl: process.env.REDIS_URL,
+        redisUrl: REDIS_URL || process.env.REDIS_URL,
       },
     },
     {
       resolve: "@medusajs/medusa/workflow-engine-redis",
       options: {
         redis: {
-          url: process.env.REDIS_URL,
+          url: REDIS_URL || process.env.REDIS_URL,
         },
       },
     },
@@ -38,8 +39,8 @@ module.exports = defineConfig({
     disable: process.env.DISABLE_MEDUSA_ADMIN === "true",
   },
   projectConfig: {
-    databaseUrl: process.env.DATABASE_URL, //: DATABASE_URL,
-    redisUrl: process.env.REDIS_URL,
+    databaseUrl: DATABASE_URL, //process.env.DATABASE_URL, //: DATABASE_URL,
+    redisUrl: REDIS_URL, // process.env.REDIS_URL,
     workerMode: process.env.MEDUSA_WORKER_MODE as "shared" | "worker" | "server",
     http: {
       storeCors: process.env.STORE_CORS!,
