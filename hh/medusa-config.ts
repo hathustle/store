739c847c927260c5e92e981f9e72471afd3942d6
Kyle Rable <Krable55@gmail.com>
2024-12-18 22:56:49 -0800

    Fix url

--- hh/medusa-config.ts
+++ hh/medusa-config.ts
@@ -1,15 +1,6 @@
 import { loadEnv, defineConfig, Modules, ModuleRegistrationName } from '@medusajs/framework/utils'
 
 loadEnv(process.env.NODE_ENV || 'development', process.cwd())
-const DB_USERNAME = process.env.DB_USERNAME
-const DB_PASSWORD = process.env.DB_PASSWORD
-const DB_HOST = process.env.DB_HOST
-const DB_PORT = process.env.DB_PORT
-const DB_DATABASE = process.env.DB_DATABASE
-
-const DATABASE_URL =
-  `postgres://${DB_USERNAME}:${DB_PASSWORD}` +
-  `@${DB_HOST}:${DB_PORT}/${DB_DATABASE}`
 
 module.exports = defineConfig({
   modules: [
@@ -18,7 +9,6 @@ module.exports = defineConfig({
       key: ModuleRegistrationName.CACHE,
       options: {
         redisUrl: process.env.REDIS_URL,
-        // serviceName: "cacheService", 
       },
     },
     {
@@ -26,7 +16,6 @@ module.exports = defineConfig({
       key: ModuleRegistrationName.EVENT_BUS,
       options: {
         redisUrl: process.env.REDIS_URL,
-        // serviceName: "eventService", 
       },
     },
     {
@@ -35,7 +24,6 @@ module.exports = defineConfig({
       options: {
         redis: {
           url: process.env.REDIS_URL,
-          // serviceName: "workflowService", 
         },
       },
     },
@@ -44,7 +32,7 @@ module.exports = defineConfig({
     disable: process.env.DISABLE_MEDUSA_ADMIN === "true",
   },
   projectConfig: {
-    databaseUrl: DATABASE_URL, // process.env.DATABASE_URL, //: DATABASE_URL,
+    databaseUrl: process.env.DATABASE_URL, //: DATABASE_URL,
     redisUrl: process.env.REDIS_URL,
     workerMode: process.env.MEDUSA_WORKER_MODE as "shared" | "worker" | "server",
     http: {
