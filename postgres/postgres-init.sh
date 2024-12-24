6d1f8f0922c99dc5ee0b02e5facb1fea5d5d3ad5
Kyle Rable <Krable55@gmail.com>
2024-12-24 12:22:31 -0800

    Update build script

--- postgres/postgres-init.sh
+++ postgres/postgres-init.sh
@@ -1,16 +1,12 @@
 #!/bin/bash
 
 # Ensure SSL certs exist before configuring
-if [ ! -f "/usr/local/share/ca-certificates/postgres.crt" ]; then
+if [ ! -f "/var/lib/postgresql/server-certs/server.crt" ]; then
     echo "SSL cert not found. Exiting..."
     exit 1
 fi
 
 # Configure Postgres to use SSL
 echo "ssl = on" >> "/var/lib/postgresql/data/postgresql.conf"
-echo "ssl_cert_file = '/usr/local/share/ca-certificates/postgres.crt'" >> "/var/lib/postgresql/data/postgresql.conf"
-echo "ssl_key_file = '/usr/local/share/ca-certificates/postgres.key'" >> "/var/lib/postgresql/data/postgresql.conf"
-
-# Set permissions
-chown -R postgres:postgres /usr/local/share/ca-certificates
-chmod 600 /usr/local/share/ca-certificates/postgres.key
+echo "ssl_cert_file = '/var/lib/postgresql/server-certs/server.crt'" >> "/var/lib/postgresql/data/postgresql.conf"
+echo "ssl_key_file = '/var/lib/postgresql/server-certs/server.key'" >> "/var/lib/postgresql/data/postgresql.conf"
