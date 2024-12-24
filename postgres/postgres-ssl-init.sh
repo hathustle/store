10c72fb4588dfb29403cd5814fda66e4e8c6d848
Kyle Rable <Krable55@gmail.com>
2024-12-24 14:34:39 -0800

    fix names

--- postgres/postgres-ssl-init.sh
+++ postgres/postgres-ssl-init.sh
@@ -5,19 +5,19 @@ SSL_DIR="/etc/postgresql/ssl"
 CERTS_DIR="/usr/local/share/ca-certificates"
 
 mkdir -p "$SSL_DIR"
-cp "$CERTS_DIR/server.crt" "$SSL_DIR/server.crt"
-cp "$CERTS_DIR/server.key" "$SSL_DIR/server.key"
+cp "$CERTS_DIR/postgres.crt" "$SSL_DIR/postgres.crt"
+cp "$CERTS_DIR/postgres.key" "$SSL_DIR/postgres.key"
 cp "$CERTS_DIR/ca.crt" "$SSL_DIR/ca.crt"
 
 # Set correct permissions for Postgres to use the certificates
-chmod 600 "$SSL_DIR/server.key"
+chmod 600 "$SSL_DIR/postgres.key"
 chown -R postgres:postgres "$SSL_DIR"
 
 # Update postgresql.conf for SSL
 cat >> /var/lib/postgresql/data/postgresql.conf <<EOF
 ssl = 'on'
-ssl_cert_file = '$SSL_DIR/server.crt'
-ssl_key_file = '$SSL_DIR/server.key'
+ssl_cert_file = '$SSL_DIR/postgres.crt'
+ssl_key_file = '$SSL_DIR/postgres.key'
 ssl_ca_file = '$SSL_DIR/ca.crt'
 EOF
 
